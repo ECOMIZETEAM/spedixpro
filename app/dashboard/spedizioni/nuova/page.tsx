07247b45fe0d9b585b510f346896f7a4a994deff
Luca <luca@ecomize.it>
2026-07-02 12:38:58 +0200

    auto-save 2026-07-02 12:38:58

--- app/dashboard/spedizioni/nuova/page.tsx
+++ app/dashboard/spedizioni/nuova/page.tsx
@@ -333,7 +333,7 @@ export default function NuovaSpedizionePage() {
                 </button>
               )}
             </div>
-          </div>
+          </div>)}
         </div>
       </div>
 
